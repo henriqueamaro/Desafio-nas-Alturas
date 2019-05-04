4f73aeab5a76ad0a3bfedeb177d7e6a5d25f877c
Felipe Henrique <felipehenriqueam@gmail.com>
2019-05-03 21:55:45 -0300

    Adicionando Awake(iniciar objeto

---
+++ Desafio nas Alturas/Assets/Scripts/Aviao.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aviao : MonoBehaviour
+{
+
+    Rigidbody2D fisica;
+
+    private void Awake()
+    {
+        
+        {
+            this.fisica = this.GetComponent<Rigidbody2D>();
+        }
+    }
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("Clicou");
+        }
+    }
+}
