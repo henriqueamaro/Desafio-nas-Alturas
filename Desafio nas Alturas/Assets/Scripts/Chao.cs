250d5941473474d1da1fab03aa865b131deb1fb3
Felipe Henrique <felipehenriqueam@gmail.com>
2019-05-10 20:31:29 -0300

    Movimentando o chão

---
+++ Desafio nas Alturas/Assets/Scripts/Chao.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chao : MonoBehaviour
+{
+
+    [SerializeField]
+
+    private float velocidade;
+
+    private Vector3 posicaoInicial;
+    private float tamanhoRealDaImagem;
+
+    private void Awake()
+    {
+        posicaoInicial = transform.localPosition;
+        float tamanhoDaImagem = GetComponent<SpriteRenderer>().size.x;
+        float escala = transform.localScale.x;
+        tamanhoRealDaImagem = tamanhoDaImagem * escala;
+        Debug.Log(tamanhoRealDaImagem);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deslocamento = Mathf.Repeat(Time.time * velocidade, tamanhoRealDaImagem);
+        transform.position = posicaoInicial + Vector3.left * deslocamento;
+    }
+}
