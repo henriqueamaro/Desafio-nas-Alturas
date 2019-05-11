6767b0fbd26e31779c416714d81a78e1506883d2
Felipe Henrique <felipehenriqueam@gmail.com>
2019-05-10 22:13:39 -0300

    Diretor (fianlização) ;

--- Desafio nas Alturas/Assets/Scripts/Aviao.cs
+++ Desafio nas Alturas/Assets/Scripts/Aviao.cs
@@ -8,22 +8,30 @@ public class Aviao : MonoBehaviour
     Rigidbody2D fisica;
     [SerializeField]
 
+    private Diretor diretor;
+
     private float forca = 10f;
     private void Awake()
     {
+             
+            
+        this.fisica = this.GetComponent<Rigidbody2D>();
+        diretor = GameObject.FindObjectOfType<Diretor>();
+
         
-        {
-            //Debug.log("Acordei")
-            this.fisica = this.GetComponent<Rigidbody2D>();
-        }
     }
 
     private void Impulsionar()
     {
+        this.fisica.velocity = Vector2.zero;
         this.fisica.AddForce(Vector2.up * forca, ForceMode2D.Impulse);
     }
 
-
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        fisica.simulated = false;
+        diretor.FinalizarJogo();
+    }
 
     void Update()
     {
--- Desafio nas Alturas/Assets/Scripts/Obstaculos.cs
+++ Desafio nas Alturas/Assets/Scripts/Obstaculos.cs
@@ -6,7 +6,28 @@ public class Obstaculos : MonoBehaviour
 {
     [SerializeField]
 
-    private float velocidade = 0.1f;
+    private float velocidade = 0.2f;
+    [SerializeField]
+
+    private float variacaoDaPosicaoY;
+
+    private void Awake()
+    {
+        transform.Translate(Vector3.up * Random.Range(-variacaoDaPosicaoY, variacaoDaPosicaoY));
+    }
+
+    private void OnTriggerEnter2D(Collider2D outro)
+    {
+        Destruir();
+    }
+
+
+    private void Destruir()
+    {
+        GameObject.Destroy(this.gameObject);
+        //Debug.Log("C");
+    }
+
     // Update is called once per frame
     void Update()
     {
